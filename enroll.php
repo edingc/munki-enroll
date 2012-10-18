c65662e0e11761af8e5f84cb6cc2772997efffab
Cody Eding <cody.eding@gmail.com>
2012-10-18 19:31:10 -0400

    Initial commit.

---
+++ enroll.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once('cfpropertylist-1.1.2/CFPropertyList.php');
+
+
+// Get the varibles past from CURL on the imaged computer
+$identifier = $_GET["identifier"];
+$hostname = $_GET["hostname"];
+
+//NEED TO SPLIT IDENTIFER SLASHES SO WE CAN GET PARENT FOLDER
+$pieces = explode("/", $identifier);
+$total = count($pieces);
+$total = $total-1;
+$n = 0;
+$identifier_path= "";
+while($n < $total){
+	$identifier_path .= $pieces[$n].'/';
+	$n++;
+}
+
+
+// Check if manifest already exists for this machine
+if (file_exists('../manifests/'.$identifier_path.'/clients/'.$hostname)) {
+	echo "Computer manifest already exists.";
+} else {
+
+
+	echo "Computer manifest does not exist. Will create.";
+	
+	if (!is_dir('../manifests/'.$identifier_path.'clients/')) {
+		mkdir('../manifests/'.$identifier_path.'clients/', 0755, true);
+	}
+
+
+	//if (!copy('template', '../manifests/'.$identifier_path.'clients/'.$hostname)) {
+    //echo "failed to copy manifest...";
+	//}
+	
+	//$plist = new CFPropertyList( '../manifests/'.$identifier_path.'clients/'.$hostname );
+	
+$plist = new CFPropertyList();	
+$plist->add( $dict = new CFDictionary() );
+
+// <key>Kids Names</key><array><string>John</string><string>Kyra</string></array>
+$dict->add( 'catalogs', $array = new CFArray() );
+$array->add( new CFString( 'production' ) );
+
+$dict->add( 'included_manifests', $array = new CFArray() );
+$array->add( new CFString( $identifier ) );
+
+$plist->saveXML( '../manifests/'.$identifier_path.'clients/'.$hostname );
+
+}
+
+?>
+
