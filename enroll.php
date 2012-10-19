a796962458f9335b1ef6a283493024491b273172
Cody Eding <cody.eding@gmail.com>
2012-10-18 22:53:08 -0400

    Cleaned up directory logic. Improved comments.

--- enroll.php
+++ enroll.php
@@ -2,22 +2,20 @@
 
 require_once( 'cfpropertylist-1.1.2/CFPropertyList.php' );
 
-// Get the varibles past from CURL on the imaged computer
+// Get the varibles passed by the enroll script
 $identifier = $_GET["identifier"];
 $hostname   = $_GET["hostname"];
 
-//NEED TO SPLIT IDENTIFER SLASHES SO WE CAN GET PARENT FOLDER
-$pieces          = explode( "/", $identifier );
-$total           = count( $pieces );
-$total           = $total - 1;
+// Split the manifest path up to determine directory structure
+$directories     = explode( "/", $identifier, -1 ); 
+$total			 = count( $directories );
 $n               = 0;
 $identifier_path = "";
 while ( $n < $total )
     {
-        $identifier_path .= $pieces[$n] . '/';
+        $identifier_path .= $directories[$n] . '/';
         $n++;
     }
-
 
 // Check if manifest already exists for this machine
 if ( file_exists( '../manifests/' . $identifier_path . '/clients/' . $hostname ) )
@@ -33,15 +31,19 @@ else
                 mkdir( '../manifests/' . $identifier_path . 'clients/', 0755, true );
             }
         
+        // Create the new manifest plist
         $plist = new CFPropertyList();
         $plist->add( $dict = new CFDictionary() );
         
+        // Add manifest to production catalog by default
         $dict->add( 'catalogs', $array = new CFArray() );
         $array->add( new CFString( 'production' ) );
         
+        // Add parent manifest to included_manifests to achieve waterfall effect
         $dict->add( 'included_manifests', $array = new CFArray() );
         $array->add( new CFString( $identifier ) );
         
+        // Save the newly created plist
         $plist->saveXML( '../manifests/' . $identifier_path . 'clients/' . $hostname );
         
     }
