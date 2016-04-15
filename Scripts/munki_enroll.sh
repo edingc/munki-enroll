7e8463bf1902175b79aca5269ac607ffb31ad752
Cody Eding <cody@codyeding.com>
2016-04-14 19:09:48 -0600

    Fix issue related to client manifest creation based root manifest.
    Fixes GitHub issue No. 5.

--- Scripts/munki_enroll.sh
+++ Scripts/munki_enroll.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
- 
+
+# Gather computer information
 IDENTIFIER=$( defaults read /Library/Preferences/ManagedInstalls ClientIdentifier ); 
 HOSTNAME=$( scutil --get ComputerName );
 
-SUBMITURL="https://munki/munki-enroll/enroll.php"
+# Change this URL to the location fo your Munki Enroll install
+SUBMITURL="http://localhost:8888/munki/munki-enroll/enroll.php"
 
 # Application paths
 CURL="/usr/bin/curl"
@@ -12,9 +14,15 @@ $CURL --max-time 5 --silent --get \
     -d hostname="$HOSTNAME" \
     -d identifier="$IDENTIFIER" \
     "$SUBMITURL"
- 	
-IDENTIFIER_PATH=$( echo "$IDENTIFIER" | sed 's/\/[^/]*$//' ); 
- 	
-defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "$IDENTIFIER_PATH/clients/$HOSTNAME"
+
+# This is a fix for clients based on a manifest in the root /manifests directory
+# See GitHub issue No. 5
+if [ $( echo "$IDENTIFIER" | grep "/" ) ]
+then
+  IDENTIFIER_PATH=$( echo "$IDENTIFIER" | sed 's/\/[^/]*$//' ); 
+  defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "$IDENTIFIER_PATH/clients/$HOSTNAME"
+else
+  defaults write /Library/Preferences/ManagedInstalls ClientIdentifier "clients/$HOSTNAME"
+fi
  
 exit 0
